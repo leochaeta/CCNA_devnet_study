872f244f18c5a340370ce391428edd473cc24cad
Raul Eduardo Flores <raul.flores@ciberc.com>
2020-02-29 13:10:10 -0600

    Agregamos un nuevo print y modificamos el primer print del script 1 de manera local

--- script1.py
+++ script1.py
@@ -1,2 +1,3 @@
 #Agregamos algo a este script
-print ("hello world script 1")
+print ("hello world script 1 y esto lo tratamos de modificar de manera local")
+print ("agregamos un nuevo print de manera local")
